93ca0760fa5bbd283332035db40338c8ba9b1d7f
Dasha Chizhova <103762177+DashaChizhova@users.noreply.github.com>
2024-02-10 19:59:54 +0300

    .

--- js/pages.js
+++ js/pages.js
@@ -4,3 +4,10 @@ fetch('include/header.html')
             .then(data => {
                 document.querySelector('nav').innerHTML = data;
 });
+
+//connecting the footer in index.html
+fetch('include/footer.html')
+    .then(response => response.text())
+    .then(data=>{
+        document.querySelector('footer').innerHTML = data;
+    });
